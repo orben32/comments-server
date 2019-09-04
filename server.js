b0b73acd2b22616e561c7e70f6f7fdd5abb64f4f
orb <orb@wix.com>
2019-09-04 22:53:41 +0300

    alloworigin

--- server.js
+++ server.js
@@ -20,8 +20,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
-  db.collection('listingsAndReviews').find().limit(10).toArray((err, result) => {
+  db.collection('listingsAndReviews', {name: 1}).find().limit(10).toArray((err, result) => {
     if (err) return console.log(err)
     res.send(result);
   })
