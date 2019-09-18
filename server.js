5e0d4c169f8df89375ec7dd42daa76fce4f3d3e2
orb <orb@wix.com>
2019-09-18 18:42:07 +0300

    clean-up

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+  res.header("Access-Control-Allow-Origin", "*") // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 app.get('/comments', (req, res) => {
-  const {instanceId} = req.query;
+  const {instanceId} = req.query
   const collection = db.collection('comments', {text: 1, createDate: 1})
   const countPromise = collection.count({instanceId})
   const itemsPromise = collection.find({instanceId}).sort({createDate: -1}).limit(10).toArray()
@@ -46,19 +46,8 @@ app.post('/comments', (req, res) => {
   })
 })
 
-app.get('/comments/count', (req, res) => {
-  console.log('count');
-  const {instanceId} = req.query;
-  
-  db.collection('comments').find({instanceId}).count(function (e, count) {
-    console.log(count)
-    res.send({count})
-  });
-})
-
-
 app.get('/comments/deleteAll', (req, res) => {
-  const {instanceId} = req.query;
+  const {instanceId} = req.query
 
   db.collection('comments', {}, function(err, contacts) {
     contacts.remove({instanceId}, function(err, result) {
@@ -67,37 +56,6 @@ app.get('/comments/deleteAll', (req, res) => {
         }
         console.log(result)
         res.send('ok')
-    });
-  })
-})
-
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-app.put('/quotes', (req, res) => {
-  db.collection('quotes')
-  .findOneAndUpdate({name: 'Yoda'}, {
-    $set: {
-      name: req.body.name,
-      quote: req.body.quote
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
-
-app.delete('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
-    if (err) return res.send(500, err)
-    res.send('A darth vadar quote got deleted')
+    })
   })
 })
