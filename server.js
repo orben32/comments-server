d841aaca73ce07d674c2c6549a4bb6519cfcbc60
orb <orb@wix.com>
2019-09-25 20:33:45 +0300

    using fetch

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const fs = require('fs')
 const JSZIP = require('jszip')
+const fetch = require('node-fetch')
 
 let db
 
@@ -44,13 +45,17 @@ app.use(function(req, res, next) {
 })
 
 app.get('/usdz.usdz', (req, res) => {
-  const zip = new JSZIP()
-  zip.file('gugi.usdc', fs.readFileSync('gugi2/gugi.usdc'))
-    .file('0/USDLogoLrg.png', fs.readFileSync('public/usdpython_0/samples/assets/textures/island_bc.png'))
-    .generateAsync({type: 'nodebuffer', compression: 'STORE'})
-    .then(zipped => {
-      res.type('model/vnd.usdz+zip')
-      res.send(zipped)
+  fetch('http://www.firstlooksagency.com/wp-content/uploads/2016/10/NJMLSBillboard.png')
+    .then(res => res.arrayBuffer())
+    .then(imageData => {
+      const zip = new JSZIP()
+      zip.file('gugi.usdc', fs.readFileSync('gugi2/gugi.usdc'))
+      .file('0/USDLogoLrg.png', imageData)
+      .generateAsync({type: 'nodebuffer', compression: 'STORE'})
+      .then(zipped => {
+        res.type('model/vnd.usdz+zip')
+        res.send(zipped)
+      })
     })
 })
 
