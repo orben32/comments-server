be3a356a99034fec352aa8a224bc2fc147cebdbc
orb <orb@wix.com>
2019-09-26 17:10:34 +0300

    fix

--- usdzUtils.js
+++ usdzUtils.js
@@ -6,9 +6,9 @@ const createFromImage = imageData => Jimp.read(imageData)
   .then(image => image.getBufferAsync(Jimp.MIME_PNG))
   .then(imageData => {
     const zip = new JSZIP()
-    zip.file('gugi.usdc', fs.readFileSync('gugi2/gugi.usdc'))
-    .file('0/USDLogoLrg.png', imageData)
-    .generateAsync({type: 'nodebuffer', compression: 'STORE'})
+    return zip.file('gugi.usdc', fs.readFileSync('gugi2/gugi.usdc'))
+      .file('0/USDLogoLrg.png', imageData)
+      .generateAsync({type: 'nodebuffer', compression: 'STORE'})
 })
 
 module.exports = {
